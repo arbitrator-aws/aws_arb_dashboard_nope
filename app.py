3284c84f0096da1e36a81ba9efeee024f6f978cc
abpmaq5 <philip.massie@absa.co.za>
2019-08-16 16:09:03 +0200

    flask test

---
+++ app.py
@@ -0,0 +1,42 @@
+# from flask import Flask, jsonify
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return jsonify({"message": "Hello World!"})
+
+# if __name__ == '__main__':
+#     app.run()
+
+from os import environ as env
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    DEBUG = False if env['STAGE'] == 'prod' else True
+    app.run_server(debug=DEBUG)
